bee64da7af21d5f983f41806b01efd0eafa1b144
SamPotasz <sampotasznik@gmail.com>
2020-04-15 17:08:29 -0500

    Updates

---
+++ precache-manifest.9b5b2a8723b7db8b39a3b4b142e178b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1786bc5bc9e14b2b26b51afb45112a7",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "379f0659f9aa1f73e912",
+    "url": "/mosters-rolodex/static/css/main.af07db1f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/mosters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mosters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379f0659f9aa1f73e912",
+    "url": "/mosters-rolodex/static/js/main.0ed6f606.chunk.js"
+  },
+  {
+    "revision": "3b1d11c5c06fc5abea79",
+    "url": "/mosters-rolodex/static/js/runtime-main.f637eaeb.js"
+  }
+]);
